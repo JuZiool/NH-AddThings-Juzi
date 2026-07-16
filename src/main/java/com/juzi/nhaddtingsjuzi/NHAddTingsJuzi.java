0f1aa064b3bfd8eb7569ffc59cf525a305c5154a
juzi <juzi@users.noreply.github.com>
2026-07-16 12:20:52 +0800

    chore: release v0.1.0b

--- src/main/java/com/juzi/nhaddtingsjuzi/NHAddTingsJuzi.java
+++ src/main/java/com/juzi/nhaddtingsjuzi/NHAddTingsJuzi.java
@@ -18,7 +18,7 @@ public class NHAddTingsJuzi
 {
     public static final String MODID = "nh_addtings_juzi";
     public static final String NAME = "NH-AddTings-Juzi";
-    public static final String VERSION = "0.0.9b";
+    public static final String VERSION = "0.1.0b";
 
     /** 飞行符咒物品实例 */
     public static Item flightCharm;
