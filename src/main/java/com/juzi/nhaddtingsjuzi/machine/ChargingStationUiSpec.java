967ce3e09067ca9e0f70167c6ae72c1b30d7f9aa
JuZiool <154224408+JuZiool@users.noreply.github.com>
2026-07-17 09:23:06 +0800

    fix: remove duplicate charging station description

--- src/main/java/com/juzi/nhaddtingsjuzi/machine/ChargingStationUiSpec.java
+++ src/main/java/com/juzi/nhaddtingsjuzi/machine/ChargingStationUiSpec.java
@@ -7,6 +7,6 @@ public final class ChargingStationUiSpec {
     private ChargingStationUiSpec() {}
 
     public static String description() {
-        return "无线为队伍成员和附近 GregTech 机器充电；电路等级决定电压、安培数和服务半径";
+        return "";
     }
 }
--- src/test/java/com/juzi/nhaddtingsjuzi/machine/ChargingStationUiSpecTest.java
+++ src/test/java/com/juzi/nhaddtingsjuzi/machine/ChargingStationUiSpecTest.java
@@ -17,10 +17,8 @@ import net.minecraft.item.ItemStack;
 public class ChargingStationUiSpecTest {
 
     @Test
-    public void describesChargingStationBehavior() {
-        assertEquals(
-                "无线为队伍成员和附近 GregTech 机器充电；电路等级决定电压、安培数和服务半径",
-                ChargingStationUiSpec.description());
+    public void doesNotAddLegacyDescription() {
+        assertEquals("", ChargingStationUiSpec.description());
     }
     @Test
     public void exposesGtItemTooltipHookAndLocalizedText() throws Exception {
