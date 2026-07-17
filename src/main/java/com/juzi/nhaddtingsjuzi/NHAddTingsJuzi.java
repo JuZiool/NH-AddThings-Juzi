a23c909c33f2a239d2113e51905698df53c4a930
JuZiool <154224408+JuZiool@users.noreply.github.com>
2026-07-17 13:23:28 +0800

    release: prepare 0.1.4b

--- src/main/java/com/juzi/nhaddtingsjuzi/NHAddTingsJuzi.java
+++ src/main/java/com/juzi/nhaddtingsjuzi/NHAddTingsJuzi.java
@@ -28,7 +28,7 @@ public class NHAddTingsJuzi
 {
     public static final String MODID = "nh_addtings_juzi";
     public static final String NAME = "NH-AddTings-Juzi";
-    public static final String VERSION = "0.1.4a";
+    public static final String VERSION = "0.1.4b";
 
     /** 本模组专属创造模式标签页 */
     public static CreativeTabs TAB_NH_ADD_TINGS = new CreativeTabs("nh_addtings_juzi") {
